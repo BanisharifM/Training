83eed8f8da8395b5c1a2324f891e2bb4ba969dbc
mahdi <mbd1377@gmail.com>
2020-04-05 12:47:57 +0430

    typescript part 7 Generics completed!

--- TypeScript/Udemy/generics/src/app.ts
+++ TypeScript/Udemy/generics/src/app.ts
@@ -17,10 +17,10 @@ function merge<T, U>(objA: T, objB: U) {
 const mergeObj = merge({ name: "Max" }, { age: 30 });
 console.log(mergeObj);
 
-interface Lenthy {
+interface Lengthy {
   length: number;
 }
-function countAndDescribe<T extends Lenthy>(element: T): [T, string] {
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
   let descriptionText = "Got no value.";
   if (element.length === 1) descriptionText = "Got 1 element.";
   else if (element.length > 1)
@@ -28,3 +28,63 @@ function countAndDescribe<T extends Lenthy>(element: T): [T, string] {
   return [element, descriptionText];
 }
 console.log(countAndDescribe("Hi there!"));
+
+function extractAndConvert<T extends object, U extends keyof T>(
+  obj: T,
+  key: U
+) {
+  return "value: " + obj[key];
+}
+console.log(extractAndConvert({ name: "Max" }, "name"));
+
+class DataStorage<T extends string | number | boolean> {
+  private data: T[] = [];
+  addItem(item: T) {
+    this.data.push(item);
+  }
+  removeItem(item: T) {
+    if (this.data.indexOf(item) === -1) {
+      return;
+    }
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+  getItem() {
+    return [...this.data];
+  }
+}
+const textStorage = new DataStorage<string>();
+textStorage.addItem("Ali");
+textStorage.addItem("Mahdi");
+textStorage.removeItem("Ali");
+console.log(textStorage.getItem());
+
+const numberStorage = new DataStorage<number>();
+//...
+
+// const objectStorage = new DataStorage<object>();
+// const maxObject = { name: "Max" };
+// objectStorage.addItem(maxObject);
+// objectStorage.addItem({ name: "Ali" });
+// objectStorage.removeItem(maxObject);
+// console.log(objectStorage.getItem());
+
+interface CourseGoal {
+  title: string;
+  description: string;
+  completeUntil: Date;
+}
+function createCourseGoal(
+  title: string,
+  description: string,
+  completeUntil: Date
+): CourseGoal {
+  let courseGoal: Partial<CourseGoal> = {};
+  courseGoal.title = title;
+  courseGoal.description = description;
+  courseGoal.completeUntil = completeUntil;
+  return courseGoal as CourseGoal;
+}
+
+const studentsNames: Readonly<string[]> = ["Mahdi", "Ali"];
+// studentsNames.push("Amir");
+// studentsNames.pop("Ali");
